080ab82db085af547049d88648727881b37be7ea
MissJuly <joygitau98@gmail.com>
2023-09-14 00:44:21 +0300

    Fix errors

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -11,9 +11,10 @@ class Config():
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
-    
+
 
 app = Flask(__name__)
+app.config.from_object(Config)
 babel = Babel(app)
 
 
